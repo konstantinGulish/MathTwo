89cde6cd2d04440555248dac27e6c544f7f72bf6
konstantinGulish <konstantin_gulish@yahoo.com>
2018-05-31 12:50:16 -0400

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int mynumber = 2;
+	double mymothernumber = 1.7938;
+        System.out.println(mynumber);
+        System.out.println(mymothernumber);
+
+    }
+}
